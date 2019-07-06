a3b557d5c7e995d5384cc48d1bd7779c4d2b7a36
Francesco Vignola <francesco.vignola@studio.unibo.it>
2019-07-06 19:16:11 +0200

    Implement a dummy  White Pages service

---
+++ src/main/scala/server/WhitePages.scala
@@ -0,0 +1,47 @@
+package server
+
+import akka.actor.{Actor, Props}
+import server.WhitePages.{DeleteUserFromChatRoom, ErrorUserNotInChatRoom, PutUserChatRoom, ReplyUsersInChat, _}
+
+
+private class WhitePages extends Actor {
+
+  private var chatRooms: Map[String, Seq[String]] = Map.empty
+
+  override def receive: Receive = {
+    case PutUserChatRoom(path: String, chatRoomName: String) =>
+      val usersInChat: Seq[String] = chatRooms.getOrElse(chatRoomName, Seq.empty)
+      sender() ! ReplyUsersInChat(chatRoomName, usersInChat)
+
+      val updateUsersInChat: Seq[String] = usersInChat :+ path
+      chatRooms = chatRooms + (chatRoomName -> updateUsersInChat)
+      usersInChat.foreach(userPath =>
+        context.actorSelection(userPath.substring(0, userPath.lastIndexOf("/"))) ! JoinedUserMessage(path))
+
+    case DeleteUserFromChatRoom(path: String, chatRoomName: String) =>
+      println("DeleteUserFromChatRoom")
+      val usersInChat: Seq[String] = chatRooms.getOrElse(chatRoomName, Seq.empty)
+
+      if (usersInChat.isEmpty) {
+        sender() ! ErrorUserNotInChatRoom("The user is not a member of this chat: " + chatRoomName)
+      } else {
+        chatRooms = chatRooms + (chatRoomName -> usersInChat)
+        sender() ! ReplyUserDeleted
+        usersInChat.foreach(user => context.actorSelection(user) ! UnJoinedUserMessage(path))
+      }
+  }
+}
+
+
+object WhitePages {
+  def props: Props = Props(new WhitePages)
+
+  final case class JoinedUserMessage(path: String)
+  final case class PutUserChatRoom(path: String, chatRoomName: String)
+  final case class DeleteUserFromChatRoom(path: String, chatRoomName: String)
+
+  final case class UnJoinedUserMessage(path: String)
+  final case class ReplyUsersInChat(chatRoomName: String, users: Seq[String])
+  final case class ErrorUserNotInChatRoom(message: String)
+  final object ReplyUserDeleted
+}
