ae2ccc16bf591a483f7dcf30591b84ca2d1161f5
Francesco Vignola <francesco.vignola@studio.unibo.it>
2019-07-07 13:23:48 +0200

    Update the UI to support multiple chat rooms

--- src/main/scala/ui/MessageActor.scala
+++ src/main/scala/ui/MessageActor.scala
@@ -1,14 +1,17 @@
 package ui
 
+import java.util.regex.Pattern
+
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import akka.stream.scaladsl.{Source, StreamConverters}
 import akka.stream.{ActorMaterializer, IOResult, Materializer}
 import akka.util.ByteString
-import user.ChatRoom.JoinInChatRoom
 import user.ChatRoom
+import user.ChatRoom.JoinInChatRoom
 import user.UserInChat.MessageInChat
 
 import scala.concurrent.Future
+import scala.util.matching.Regex
 
 private class MessageActor extends Actor with ActorLogging {
 
@@ -16,50 +19,48 @@ private class MessageActor extends Actor with ActorLogging {
 
   implicit val materializer: Materializer = ActorMaterializer()
 
-  private var usersInChat: Option[(String, ActorRef)] = Option.empty
+  private var usersInChat: Map[String, ActorRef] = Map.empty
 
   private val stdinSource: Source[ByteString, Future[IOResult]] = StreamConverters.fromInputStream(() => System.in)
   stdinSource.map(text => text.utf8String).runForeach(sendTypedText)
 
-  override def receive: Receive = joining
+  override def receive: Receive = {
+    case text: String if text.contains("@") && text.contains(":") =>
+      val colonIndex = text.indexOf(":")
+      val receiver = usersInChat.get(text.substring(1, colonIndex))
+      if (receiver.isDefined) receiver.get ! MessageInChat(text.substring(colonIndex + 1))
+      else showErrorMessage("Insert the correct chat room name")
 
-  private def sendTypedText(content: String): Unit = self ! content
+    case usernameAndChatRoomName: String if usernameAndChatRoomName.contains("@") =>
+      val usernameAndChatroomNameSplit = usernameAndChatRoomName.trim.split("@")
+      val chatroom = context.actorOf(ChatRoom.props(usernameAndChatroomNameSplit.head, self), name = usernameAndChatroomNameSplit.tail.head)
+      chatroom ! JoinInChatRoom(usernameAndChatroomNameSplit.tail.head)
 
-  private def joining: Receive = {
-    case input: String if input.contains("@") =>
-      val data = input.trim.split("@")
-      val joiningActor = context.actorOf(ChatRoom.props, name = data.tail.head)
-      joiningActor ! JoinInChatRoom(data.head, data.tail.head)
-      context.watch(joiningActor)
-
-    case JoinedInChat(username: String, chatRoom: String, actor: ActorRef) =>
-      usersInChat = Some(chatRoom -> actor)
+    case ShowWelcomeMessage(username: String, chatRoom: String, actor: ActorRef) =>
+      usersInChat = usersInChat + (chatRoom -> actor)
       println(s"Welcome in $chatRoom $username")
-      context.become(joined)
+
+    case ShowMessage(content, sender) => println(s"$sender: $content")
 
     case ErrorJoin(errorText) =>
       showErrorMessage(errorText)
       showJoinMessage()
-
-    case _ =>
+/*
+    // TODO Uncomment only for tester actor for debug
+    case text: String if text.startsWith("test:") =>
+      val atIndex = text.indexOf("@")
+      val colonIndex = text.indexOf(":")
+      val username = text.substring(colonIndex + 1, atIndex)
+      val content = text.substring(atIndex + 1)
+      usersInChat.head._2 ! TestMessage(username, content)
+*/
+    case t: String =>
+      println(new Regex("^[a-zA-Z0-9]+@[a-zA-Z0-9]+$").matches(t))
       showErrorMessage("enter correct data!")
       showJoinMessage()
   }
 
-  private def joined: Receive = {
-    case ShowMessage(content, sender) => println(s"$sender: $content")
-
-/* Uncomment only for tester actor for debug
-    case text: String if text.startsWith("test:") =>
-      val username = text.substring(text.indexOf(":") + 1, text.indexOf("@"))
-      val content = text.substring(text.indexOf("@") + 1)
-      println(username)
-      println(content)
-      usersInChat.head._2 ! TestMessage(username, content)
-*/
-    case text: String if usersInChat.nonEmpty => usersInChat.head._2 ! MessageInChat(text)
-
-  }
+  private def sendTypedText(content: String): Unit = self ! content
 
   private def showJoinMessage(): Unit =
     println("Enter your username and chat-room name as <username@chatroomname>")
@@ -73,7 +74,7 @@ object MessageActor {
   def props: Props = Props(new MessageActor)
 
   final case class ShowMessage(content: String, sender: String)
-  final case class JoinedInChat(username: String, chatRoom: String, actor: ActorRef)
+  final case class ShowWelcomeMessage(username: String, chatRoom: String, actor: ActorRef)
   final case class ErrorJoin(errorText: String)
 
   final case class TestMessage(username: String, content: String)
